cbcd0cefc084688a202184c0c7e64f7329bab5e8
mohammad hossein khojoii <m.khojoii@gmail.com>
2026-08-07 10:13:56 +0330

    move: relocate Sales_item.h to practice/classes and add note (reorg/readme-practice-days)

--- practice/days/day-8/Sales_item.h
+++ practice/days/day-8/Sales_item.h
@@ -1,33 +1,7 @@
-# practice/days/day-6/user_range_improved.cpp
+# practice/days/day-8/Sales_item.h
 
-#include <iostream>
+This file was moved to practice/classes/Sales_item.h. Include it from examples using:
 
-int main()
-{
-    int temp_number = 0;
-    int start_num = 0;
-    int end_num = 0;
-    
-    std::cout << "enter start and end number for print :" << std::endl;
-    std::cin >> start_num;
-    std::cin >> end_num;
-    /*
-    if the user enters a starting number greater than the ending number,
-    swap them so the range is always processed correctly
-    */
-
-    if (start_num > end_num)
-    {
-        temp_number = start_num;
-        start_num = end_num;
-        end_num = temp_number;
-    }
-
-    std::cout << "------numbers------" << std::endl;
-
-    while (start_num <= end_num)
-    {
-        std::cout << start_num <<std::endl;
-        ++start_num;
-    }
-}
+```cpp
+#include "../../classes/Sales_item.h"
+```
